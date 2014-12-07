078861fff037d3acf47b73aba0ad0cca78f5046d
Ming Liu <nongnongd@yahoo.com>
2014-12-07 00:46:45 -0500

    Added balance entry list

--- app/src/main/java/com/mcode/balancetracker/BalanceTracker.java
+++ app/src/main/java/com/mcode/balancetracker/BalanceTracker.java
@@ -3,14 +3,19 @@ package com.mcode.balancetracker;
 import com.mcode.balancetracker.com.mcode.balancetracker.data.BalanceEntry;
 import com.mcode.balancetracker.com.mcode.balancetracker.data.BalanceSummary;
 
+import java.util.LinkedList;
+import java.util.List;
+
 /**
  * Created by Ming on 12/6/2014.
  */
 public class BalanceTracker {
+
+    private List<BalanceEntry> balanceEntryList = new LinkedList<BalanceEntry>();
     public BalanceSummary generateBalanceSummary() {
         return null;
     }
     public void insertEntry(BalanceEntry entry) {
-
+        balanceEntryList.add(entry);
     }
 }
